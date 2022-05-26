10c8cbf9f771c0bd6ead1b2a04b6546e27b25513
shuangxi <double8295@gmail.com>
2022-05-26 16:51:28 +0700

    flx_

--- example/src/layouts/default.vue
+++ example/src/layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <div><router-link to="/index">首页</router-link></div>
-        <div><router-link to="/setting/index">设置页</router-link></div>
-        <!-- <div><router-link to="/setting/23/67">设置参数</router-link></div> -->
-        <div><router-link to="/user/28783/98767">user</router-link></div>
-        <div><router-link to="/login">Login 不需要权限 走 layouts/noAuth.vue</router-link></div>
+        <br />
+        <br />
+        <router-link class="link" to="/index">首页</router-link>
+        <router-link class="link" to="/setting/index">设置页</router-link>
+        <router-link class="link" to="/user/28783/98767">user</router-link>
+        <router-link class="link" to="/login">Login 不需要权限 走 layouts/noAuth.vue</router-link>
+        <br />
+        <br />
         <slot></slot>
     </div>
 </template>
+
+<style scoped>
+.link {
+    padding: 5px 15px;
+    background: #0673fa;
+    margin: 15px;
+    color: #fff;
+}
+</style>
--- example/src/pages/user/_.vue
+++ example/src/pages/user/_.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        user/index
+        user
     </div>
 </template>
 
