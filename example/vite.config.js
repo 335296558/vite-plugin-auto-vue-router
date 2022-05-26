4fb0c4ac3c713df83de02e5d396c253f6d93f2e9
shuangxi <double8295@gmail.com>
2022-05-26 20:56:21 +0700

    flx_

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitePluginAutoVueRouter from 'vite-plugin-auto-vue-router/dist/globEager'
-import vitePluginVueLayouts from '../../vite-plugin-vue-layouts/dist/index.js'
+// import vitePluginVueLayouts from '../../vite-plugin-vue3-layouts/dist/index.js'
+import vitePluginVueLayouts from 'vite-plugin-vue3-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
         vitePluginAutoVueRouter(),
         vitePluginVueLayouts({
             plugins:[
-                'auto-vue-router'
+                'auto-vue-router',
             ]
         })
     ],
