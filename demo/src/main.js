c53988fd8897f8391e9ca2afa2e838cde974a912
凡尘 <335296558@qq.com>
2023-12-22 00:39:35 +0700

    flx

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
 
 import App from './App.vue';
-import { createWebHashHistory, createWebHistory,  } from 'vue-router';
 
 const VueApp = createApp(App);
 
 import AutoVueRouter from 'virtual:auto-vue-router';
 
-VueApp.use(AutoVueRouter, {
-    history: 'h5'
-});
+VueApp.use(AutoVueRouter);
 
 VueApp.mount('#app');
